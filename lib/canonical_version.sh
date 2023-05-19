f0a53f5c338438efcb012f310805e6a8b8160249
Alejandro Donis <alejandro.donis@gmail.com>
2023-05-18 23:15:20 -0400

    hardwire to ubuntu 22

--- lib/canonical_version.sh
+++ lib/canonical_version.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
 erlang_builds_url() {
-  case "${STACK}" in
-    "heroku-20")
-      erlang_builds_url="https://repo.hex.pm/builds/otp/ubuntu-20.04"
-      ;;
-    "heroku-22")
-      erlang_builds_url="https://repo.hex.pm/builds/otp/ubuntu-22.04"
-      ;;
-    *)
-      erlang_builds_url="https://s3.amazonaws.com/heroku-buildpack-elixir/erlang/cedar-14"
-      ;;
-  esac
+  erlang_builds_url="https://repo.hex.pm/builds/otp/ubuntu-22.04"
   echo $erlang_builds_url
 }
 
@@ -21,20 +11,8 @@ fetch_elixir_versions() {
 }
 
 fetch_erlang_versions() {
-  case "${STACK}" in
-    "heroku-20")
-      url="https://repo.hex.pm/builds/otp/ubuntu-20.04/builds.txt"
-      curl -s "$url" | awk '/^OTP-([0-9.]+ )/ {print substr($1,5)}'
-      ;;
-    "heroku-22")
       url="https://repo.hex.pm/builds/otp/ubuntu-22.04/builds.txt"
-      curl -s "$url" | awk '/^OTP-([0-9.]+ )/ {print substr($1,5)}'
-      ;;
-    *)
-      url="https://raw.githubusercontent.com/HashNuke/heroku-buildpack-elixir-otp-builds/master/otp-versions"
-      curl -s "$url"
-      ;;
-  esac
+      curl -s "$url" | awk '/^OTP-([0-9.]+ )/ {print substr($1,5)}' 
 }
 
 exact_erlang_version_available() {
